0f0dca8c692c7e5ef7036b642ba3caaa1398beb5
wgtux <weslengomes@gmail.com>
2017-01-03 17:49:22 -0200

    exercicios

--- CursoLoianeGroner/Aula15/src/ExDoze.java
+++ CursoLoianeGroner/Aula15/src/ExDoze.java
@@ -1,9 +1,70 @@
+//FOLHA DE PAGAMENTO
+
+import java.util.Scanner;
 
 public class ExDoze {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Qual o valor da sua hora trabalhada: ");
+		double vt = scan.nextDouble();
+		System.out.println("Quantas horas trabalha no mês: ");
+		double ht = scan.nextDouble();
+		
+		double salarioBruto = vt*ht;
+		double sindicato = salarioBruto * 0.03;
+		double fgts = salarioBruto * 0.11;
+		double inss = salarioBruto * 0.1;
+		
+		if (salarioBruto <=900){
+			double desconto = sindicato+inss;
+			double salarioLiquido = salarioBruto - desconto;
+			System.out.println("Salario Bruto       : R$ "+salarioBruto);
+			System.out.println("(-) IR Insento      : R$ 00.0");
+			System.out.println("(-) INSS            : R$ "+inss);
+			System.out.println("FGTS                : R$ "+fgts);
+			System.out.println("(-) Sindicato       : R$ "+sindicato);
+			System.out.println("Total de Desconto   : R$ "+desconto);
+			System.out.println("Salario Liquido     : R$ "+salarioLiquido);
+		}
+		else if(salarioBruto>900 && salarioBruto <=1500){
+			double ir = salarioBruto*0.05;
+			double desconto = sindicato+inss + ir;
+			double salarioLiquido = salarioBruto - desconto;
+			System.out.println("Salario Bruto       : R$ "+salarioBruto);
+			System.out.println("(-) IR Insento      : R$ "+ir);
+			System.out.println("(-) INSS            : R$ "+inss);
+			System.out.println("FGTS                : R$ "+fgts);
+			System.out.println("(-) Sindicato       : R$ "+sindicato);
+			System.out.println("Total de Desconto   : R$ "+desconto);
+			System.out.println("Salario Liquido     : R$ "+salarioLiquido);
+		}
+		else if (salarioBruto>1500 && salarioBruto <=2500){
+			double ir = salarioBruto*0.1;
+			double desconto = sindicato+inss+ir;
+			double salarioLiquido = salarioBruto - desconto;
+			System.out.println("Salario Bruto       : R$ "+salarioBruto);
+			System.out.println("(-) IR Insento      : R$ "+ir);
+			System.out.println("(-) INSS            : R$ "+inss);
+			System.out.println("FGTS                : R$ "+fgts);
+			System.out.println("(-) Sindicato       : R$ "+sindicato);
+			System.out.println("Total de Desconto   : R$ "+desconto);
+			System.out.println("Salario Liquido     : R$ "+salarioLiquido);
+		}
+		else{
+			double ir = salarioBruto*0.2;
+			double desconto = sindicato+inss+ir;
+			double salarioLiquido = salarioBruto - desconto;
+			System.out.println("Salario Bruto       : R$ "+salarioBruto);
+			System.out.println("(-) IR Insento      : R$ "+ir);
+			System.out.println("(-) INSS            : R$ "+inss);
+			System.out.println("FGTS                : R$ "+fgts);
+			System.out.println("(-) Sindicato       : R$ "+sindicato);
+			System.out.println("Total de Desconto   : R$ "+desconto);
+			System.out.println("Salario Liquido     : R$ "+salarioLiquido);
+		}
+		
 	}
 
 }
---
+++ CursoLoianeGroner/Aula15/src/ExTreze.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//DIAS DA SEMANA
+
+public class ExTreze {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite um numero correspondende ao dia da semana ");
+		System.out.println("Ex: 1-Domingo, 2-Segunda, 3-Terça, etc");
+		int v = scan.nextInt();
+		
+		switch(v){
+			case 1: System.out.println("Domingo");break;
+			case 2: System.out.println("Segunda Feira");break;
+			case 3: System.out.println("Terça Feira");break;
+			case 4: System.out.println("Quarta Feira");break;
+			case 5: System.out.println("Quinta Feira");break;
+			case 6: System.out.println("Sexta Feira");break;
+			case 7: System.out.println("Sabado");break;
+			default: System.out.println("Valor Invalido");
+		}
+
+	}
+
+}
