d1a982d5d3b8ef655c961b2d28ec72cab7338b3c
wgtux <weslengomes@gmail.com>
2017-01-11 18:00:13 -0200

    Aprimorando

--- Impacta/Construtor/src/Conta.java
+++ Impacta/Construtor/src/Conta.java
@@ -3,14 +3,20 @@ public class Conta {
 	private int numero;
 	private String nome;
 	private double saldo;
+	private double deposito;
+	private double saquar;
+	
 	
 	/*METODO CONSTRUTOR*/
-	public Conta (int n, String no, double s){
+	public Conta (int n, String no, double d){
 		numero = n;
 		nome = no;
-		saldo = s;
+		deposito = d;
+		
 	}
 	
+		
+		
 	/*METODOS GET AND SETTERS*/
 	public int getNumero() {
 		return numero;
@@ -26,6 +32,21 @@ public class Conta {
 	}
 	public double getSaldo() {
 		return saldo;
+	}
+	public double getDeposito() {
+		return deposito;
+	}
+
+	public void setDeposito(double deposito) {
+		this.deposito = deposito;
+	}
+
+	public double getSaquar() {
+		return saquar;
+	}
+
+	public void setSaquar(double saquar) {
+		this.saquar = saquar;
 	}
 	
 	public String getDados(){
--- Impacta/Construtor/src/TesteConta.java
+++ Impacta/Construtor/src/TesteConta.java
@@ -1,15 +1,51 @@
+import java.util.Scanner;
 
 public class TesteConta {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("#### BEM VINDO AO BANCO WGTUX ####");
+		System.out.println("----------------------------------");
+		System.out.println("MENU");
+		System.out.println("1-Abrir Conta");
+		System.out.println("2-Consultar Saldo");
+		System.out.println("3-Deposito");
+		System.out.println("4-Excluir Conta");
+		System.out.println("-------------------");
+		System.out.println("Digite sua Opção: ");
+		int e = scan.nextInt();
+		
+		
+		switch (e){
+			case 1:
+				System.out.println("Digite seu Nome: ");
+				String nome = scan.next();
+				System.out.println("Inserir valor de deposito inicial: ");
+				double dep = scan.nextDouble();
+				int n = 1000;
+				Conta c = new Conta(n,nome,dep);
+				n=n+1;
+				System.out.println(c.getDados());
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			default:
+				System.out.println("Opção Invalida!");	
+		}
+		
+		/*
 		Conta c1 = new Conta (0001, "Weslen", 2000.00);
 		Conta c2 = new Conta (0002, "Bertano", 1600.00);
 		Conta c3 = new Conta (0003, "Fulano", 1800.00);
 
 		System.out.println(c1.getDados());
-		System.out.println(c2.getDados());
-		System.out.println(c3.getDados());
+		System.out.println(c2.getDados());*/
 	}
 
 }
